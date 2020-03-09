ac6729889296fa7fb1d9155f3b8c20b9fcc4e3e8
ronschaeffer <ron@ronschaeffer.com>
2020-03-09 17:40:22 +0000

    Update sb2mqtt_config.py
    Formatted with Black

--- sb2mqtt_config.py
+++ sb2mqtt_config.py
@@ -4,10 +4,10 @@
 # Place in same folder as sbm2mqtt.py
 
 # MQTT settings
-mqtt_host = 'xxx.xxx.xxx.xxx'
+mqtt_host = "xxx.xxx.xxx.xxx"
 mqtt_port = xxxx
 mqtt_timeout = 30
-mqtt_client = 'switchbot_meter_checker'
-mqtt_user = 'xxxxxx'
-mqtt_pass = 'xxxxxx'
-mqtt_topic = 'switchbot_meter/'
+mqtt_client = "switchbot_meter_checker"
+mqtt_user = "xxxxxx"
+mqtt_pass = "xxxxxx"
+mqtt_topic = "switchbot_meter/"
